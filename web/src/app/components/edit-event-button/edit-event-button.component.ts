d7633b59120ada4e7655a2f84e8a3c8c2aa87e10
Bulat Kidasov <kidasov@gmail.com>
2019-08-31 02:05:09 +0400

    Add begin / end date fields to event edit popup

--- web/src/app/components/edit-event-button/edit-event-button.component.ts
+++ web/src/app/components/edit-event-button/edit-event-button.component.ts
@@ -5,6 +5,7 @@ import { DetailedEvent } from 'app/models/event';
 import { EventService } from 'app/services/event';
 import { StorageService, UploadEventType } from 'app/services/storage';
 import $ from 'jquery';
+import * as moment from 'moment';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -23,6 +24,12 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
   controls = {
     name: new FormControl(),
     description: new FormControl(),
+    pair: new FormControl(),
+    publish: new FormControl(),
+    startsAtDate: new FormControl(),
+    startsAtTime: new FormControl(),
+    endsAtDate: new FormControl(),
+    endsAtTime: new FormControl(),
   };
 
   editForm = new FormGroup(this.controls);
@@ -45,6 +52,39 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  get startsAtDate(): string | undefined {
+    if (this.currentEvent == null || this.currentEvent.startsAt == null) {
+      return;
+    }
+    return moment(this.currentEvent.startsAt).format('YYYY-MM-DDTHH:mm');
+  }
+
+  get endsAtDate(): string | undefined {
+    if (this.currentEvent == null || this.currentEvent.endsAt == null) {
+      return;
+    }
+    return moment(this.currentEvent.endsAt).format('YYYY-MM-DDTHH:mm');
+  }
+
+  get enteredStartsAt(): Date | undefined {
+    return this.parseDate(this.controls.startsAtDate.value || this.startsAtDate
+    );
+  }
+
+  get enteredEndsAt(): Date | null {
+    return this.parseDate(
+      this.controls.endsAtDate.value || this.endsAtDate
+    );
+  }
+
+  private parseDate(date: string | null): Date | null {
+    if (date == null) {
+      return null;
+    }
+    const parsed = moment(`${date}`).toDate();
+    return !Number.isNaN(parsed.getDate()) ? parsed : null;
+  }
+
   get editCoverPreviewStyle() {
     const url = this.uploading
       ? this.previewCoverImage
@@ -55,6 +95,14 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     };
+  }
+
+  get published() {
+    return this.currentEvent.state === 'published';
+  }
+
+  get paired() {
+    return this.currentEvent.type === 'pair';
   }
 
   upload(event: Event) {
@@ -93,6 +141,8 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
   saveEvent() {
     const name = this.controls.name.value;
     const description = this.controls.description.value;
+    const pair = this.controls.pair.value ? 'pair' : 'single';
+    const publish = this.controls.publish.value ? 'published' : 'draft'; 
     const data = {};
     if (name !== null) {
       data['name'] = name;
@@ -103,6 +153,14 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
     if (this.coverMediaId !== this.currentEvent.coverMediaId) {
       data['coverMediaId'] = this.coverMediaId;
     }
+    if (pair !== this.currentEvent.type) {
+      data['type'] = pair;
+    }
+    if (publish !== this.currentEvent.state) {
+      data['state'] = publish;
+    }
+    data['startsAt'] = this.enteredStartsAt;
+    data['endsAt'] = this.enteredEndsAt;
     this.eventService.patchEvent(this.currentEvent.id, data).subscribe(() => {
       $('#editEventModal').modal('hide');
     });
