4a48ca0ee9974d68e8e003308510906dcbf33d88
Bulat Kidasov <kidasov@gmail.com>
2019-08-31 00:08:25 +0400

    Add edit / delete image buttons to edit event popup

--- web/src/app/components/edit-event-button/edit-event-button.component.ts
+++ web/src/app/components/edit-event-button/edit-event-button.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { API_URL } from 'app/consts/common';
 import { DetailedEvent } from 'app/models/event';
 import { EventService } from 'app/services/event';
+import { StorageService, UploadEventType } from 'app/services/storage';
 import $ from 'jquery';
 import { Subscription } from 'rxjs';
 
@@ -13,6 +15,11 @@ import { Subscription } from 'rxjs';
 export class EditEventButtonComponent implements OnInit, OnDestroy {
   subscription = new Subscription();
   currentEvent: DetailedEvent;
+  coverMediaId: number;
+  previewCoverImage: string = null;
+  uploading = false;
+  uploadProgress = 0;
+
   controls = {
     name: new FormControl(),
     description: new FormControl(),
@@ -20,7 +27,7 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
 
   editForm = new FormGroup(this.controls);
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private storageService: StorageService) {}
 
   ngOnInit() {
     $('.modal').appendTo('body');
@@ -28,12 +35,59 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
       $('#event-name').focus();
     });
     this.subscription.add(
-      this.eventService.currentEvent.subscribe(event => { this.currentEvent = event; })
+      this.eventService.currentEvent.subscribe(event => { this.currentEvent = event;
+      this.coverMediaId = event.coverMediaId;
+    })
     );
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  get editCoverPreviewStyle() {
+    const url = this.uploading
+      ? this.previewCoverImage
+      : `${API_URL}/storage/${this.coverMediaId}`;
+    return {
+      backgroundImage: `url('${url}')`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    };
+  }
+
+  upload(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    if (!files.length) {
+      return;
+    }
+    const file = files[0];
+
+    const reader = new FileReader();
+    reader.addEventListener('loadend', () => {
+      this.previewCoverImage = reader.result as string;
+    });
+    reader.readAsDataURL(file);
+
+    this.uploading = true;
+    this.uploadProgress = 0;
+
+    return this.storageService.uploadFile(file).subscribe(uploadEvent => {
+      switch (uploadEvent.type) {
+        case UploadEventType.Progress:
+          this.uploadProgress = uploadEvent.progress;
+          break;
+        case UploadEventType.Complete:
+          this.uploading = false;
+          this.coverMediaId = uploadEvent.medias[0].id;
+          break;
+      }
+    });
+  }
+
+  removeCover() {
+    this.coverMediaId = null;
   }
 
   saveEvent() {
@@ -46,8 +100,16 @@ export class EditEventButtonComponent implements OnInit, OnDestroy {
     if (description !== null) {
       data['description'] = description;
     }
+    if (this.coverMediaId !== this.currentEvent.coverMediaId) {
+      data['coverMediaId'] = this.coverMediaId;
+    }
     this.eventService.patchEvent(this.currentEvent.id, data).subscribe(() => {
       $('#editEventModal').modal('hide');
     });
+  }
+
+  handleFileButtonClick() {
+    $('#file-input').click();
+    return false;
   }
 }
